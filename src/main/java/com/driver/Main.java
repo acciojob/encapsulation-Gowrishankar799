b2746eada01b00f97cf693ba6ce9e4fc8bbad08d
Gowrishankar799 <130752694+Gowrishankar799@users.noreply.github.com>
2023-06-11 10:42:01 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[]){
+        RWOnly one = new RWOnly();
+       // one.name = "venu";
+        //System.out.println(one.name);
+        one.setName("venu");
+        System.out.println(one.getName());
+
+    }
   
 }
